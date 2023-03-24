7f9265f33e93631e653021ed2d5be7c9c41d087f
Gabriel Pereyra R <pereyrarg11@gmail.com>
2023-03-23 20:25:24 -0600

    chore: creating columns and rows

--- app/src/main/java/com/pereyrarg11/codelabbasiclayouts/MainActivity.kt
+++ app/src/main/java/com/pereyrarg11/codelabbasiclayouts/MainActivity.kt
@@ -3,8 +3,8 @@ package com.pereyrarg11.codelabbasiclayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,27 +19,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CodelabBasicLayoutsTheme {
-                MyApp(Modifier.fillMaxSize())
+                MyApp(Modifier.fillMaxSize(), listOf("Gabriel", "Miguel", "Israel"))
             }
         }
     }
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) {
-        Greeting(name = "Gabriel")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Kotlin", "Android")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        names.forEach { name ->
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = {}
+            ) {
+                Text(text = "Show More")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     CodelabBasicLayoutsTheme {
